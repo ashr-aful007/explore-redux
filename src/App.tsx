5127ec686bff208aea45a2afdff65679f76bb4a6
ashr-aful007 <ashraful99980@gmail.com>
2024-03-20 23:04:59 +0600

    add inital store

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import './App.css'
 function App() {
  
   return (
-    <>
-     <h1>Hello from the other side</h1>
-    </>
+    <div className='h-screen w-full flex justify-center items-center'>
+     <div>
+       <div className='flex border border-purple-400 rounded-md bg-slate-50 p-10'>
+          <button className='px-3 py-2 rounded-md bg-green-500 text-xl 
+          font-semibold text-white'>Increment</button>
+            <h1 className='text-3xl mx-10'>0</h1>
+          <button className='px-3 py-2 rounded-md bg-red-500 text-xl 
+          font-semibold text-white'>Increment</button>
+       </div>
+     </div>
+    </div>
   )
 }
 
