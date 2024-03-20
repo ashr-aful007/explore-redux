601d0e15a6b6b0bbedcda84c4d82ebf9d2bdfd0e
ashr-aful007 <ashraful99980@gmail.com>
2024-03-20 23:37:48 +0600

    add basic action with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
+import { decrement, increment } from './redux/features/counterSlice'
 
 function App() {
+  const {count} = useSelector((state) => state.counter)
+   const dispatch = useDispatch()
  
   return (
     <div className='h-screen w-full flex justify-center items-center'>
      <div>
        <div className='flex border border-purple-400 rounded-md bg-slate-50 p-10'>
-          <button className='px-3 py-2 rounded-md bg-green-500 text-xl 
+          <button 
+            onClick={() => dispatch(increment())}
+           className='px-3 py-2 rounded-md bg-green-500 text-xl 
           font-semibold text-white'>Increment</button>
-            <h1 className='text-3xl mx-10'>0</h1>
-          <button className='px-3 py-2 rounded-md bg-red-500 text-xl 
+            <h1 className='text-3xl mx-10'>{count}</h1>
+          <button
+            onClick={() => dispatch(decrement())}
+           className='px-3 py-2 rounded-md bg-red-500 text-xl 
           font-semibold text-white'>Increment</button>
        </div>
      </div>
